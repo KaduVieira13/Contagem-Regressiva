483f89219a4c04c07ff5c0976e87775851ddf227
carlos eduardo <carloseduardo_alencar@outlook.com>
2021-03-05 11:29:26 -0300

    Contagem regressiva finalizada.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace Contagem_Regressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         string nome;
+         Console.WriteLine("Olá! Digite seu nome...");
+         nome = Console.ReadLine();
+         Console.WriteLine();
+         Console.WriteLine("Seja Bem-vindo " + nome );
+         Console.WriteLine("A contagem regressiva será iniciada. \n digite a tecla ENTER...");
+         Console.ReadKey();
+         Console.WriteLine(5);
+         Console.ReadKey();
+         Console.WriteLine(4);
+         Console.ReadKey();
+         Console.WriteLine(3);
+         Console.ReadKey();
+         Console.WriteLine(2);
+         Console.ReadKey();
+         Console.WriteLine(1);
+         Console.ReadKey();
+         Console.WriteLine("................................");
+         Console.WriteLine("................................");
+         Console.WriteLine("................................");
+         Console.WriteLine();
+         Console.WriteLine("Protocolo de auto-destruição ativado...");
+         Console.WriteLine();
+         Console.WriteLine(@"
+         
+     _.-^^---....,,--
+ _--                  --_
+<                        >)
+|                         |
+ \._                   _./
+    ```--. . , ; .--'''
+          | |   |
+       .-=||  | |=-.
+       `-=#$%&%$#=-'
+          | ;  :|
+ _____.,-#%&$@%#&#~,._____  
+
+         ");
+                    
+
+        
+           
+
+        }
+    }
+}
